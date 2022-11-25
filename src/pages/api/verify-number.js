f8925e54a887ee1267ce79fccd12fbfbdcfce573
Flavio Li Volsi <flavio.livolsi@gmail.com>
2022-11-25 11:38:01 +0100

    Twilio lookup

--- src/pages/api/verify-number.js
+++ src/pages/api/verify-number.js
@@ -1,7 +1,17 @@
+const client = require('twilio')(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+)
+
 export default function handler(req, res) {
-  if (req.query.phone.length < 13) {
-    return res.status(500).json({ error: true })
-  } else {
-    return res.status(200).json({})
-  }
+  client.lookups.v1
+    .phoneNumbers(req.query.phoneNumber)
+    .fetch({ type: ['carrier'] })
+    .then(() => {
+      res.status(200).json({ valid: true })
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ valid: false })
+    })
 }
